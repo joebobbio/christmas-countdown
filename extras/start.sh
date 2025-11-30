2337379a73b459f3269490865100370862a73024
phene <60532517+joebobbio@users.noreply.github.com>
2025-11-30 12:21:52 -0500

    Update chromium path

--- extras/start.sh
+++ extras/start.sh
@@ -2,4 +2,4 @@ xrandr -o left
 xset s noblank
 xset s off
 xset -dpms
-/usr/lib/chromium-browser/chromium-browser --force-renderer-accessibility --enable-remote-extensions --enable-pinch --enable-crashpad --start-fullscreen file:///home/pi/countdown.html
+/usr/bin/chromium --force-renderer-accessibility --enable-remote-extensions --enable-pinch --enable-crashpad --start-fullscreen file:///home/pi/christmas-countdown/countdown.html
